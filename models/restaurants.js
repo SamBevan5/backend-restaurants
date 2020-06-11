48f9759e413589ccd7612e21447bf4e677306e96
mikehoods <62457440+mikehoods@users.noreply.github.com>
2020-06-11 10:49:48 -0500

    adjusted restaurant  model to reflect API structure

--- models/restaurants.js
+++ models/restaurants.js
@@ -1,16 +1,18 @@
 const {Schema, model} = require('mongoose')
 
 const restaurantSchema = new Schema({
-  id: Number,
-  name: String,
-  url: String,
-  location: {
-    address: String,
-    locality: String,
-    city: String,
-    zipcode: Number,
+  restaurant: {
+    cuisines: String,
+    name: String,
+    thumb: String,
+    url: String,
+    location: {
+      address: String,
+      locality: String,
+      city: String,
+      zipcode: Number,
     },
-  featured_image: String
+  }
 })
 
 const Restaurant = model('restaurant', restaurantSchema)
